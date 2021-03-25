654cc08842e2732f9acd11017d7c52d7fece1157
gustavoaraujo086@gmail.com <gustavoaraujo086@gmail.com>
2021-03-25 16:12:34 -0300

    Modificações do layout e explicações sobre o mesmo

--- testedash.py
+++ testedash.py
@@ -15,8 +15,21 @@ linha1 = go.Scatter( # todos os dados dentro do comando go.Scatter() receberão 
 
 grafico = go.Figure(linha1) # tranforma a lista("linha1") em uma Figure
 
-x = 4**(1/2)
-print(x)
+grafico.update_layout( #Usa-se esse comando para modificar o layout de forma mais "separada"
+    title = dict( #Utiliza-se "title" pra adicionar um título e dict(), para adicionar mais de uma característica ao title
+        text = "Título do Gráfico", #Uma das "características" de title, é o texto que será exibido
+        font = dict( #"font" também possui mais de um atributo, assim, também usa-se o dict()
+            family = "Arial",
+            size = 45,
+            color = "black"),
+        xref = "paper", # paper é a área refente ao fundo do gráfico (cinza/slategray)
+        yref = "container", # container é a uma área em que o gráfico está contido (branco/snow)
+        x = 0.5, #posição do elemento com relação ao eixo X, na área escolhida no "xref"
+        y = 0.9 #posição do elemento com relação ao eixo Y, na área escolhida no "yref"
+    ),
+    paper_bgcolor = "slategray", #cor do background refente ao paper
+    plot_bgcolor = 'snow' #cor do background refente ao container
+)
 
 import dash
 import dash_core_components as dcc 
